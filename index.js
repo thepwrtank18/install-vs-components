6ceb00b9faaa19debef0024be8b84b414cd21a5b
TheTank20 <leoholdiman7@gmail.com>
2023-08-11 14:31:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-  const componentArray = core.getInput("components").split(",");
-  const argumentArray = ["modify", "--installPath", "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise", "--quiet", "--norestart", "--nocache"];
+let trialCount = 0;
 
-  componentArray.forEach(element => {
-    console.log(`Adding ${element} to array...`);
-    argumentArray.push("--add", element);
-  });
-
-  console.log("Launching installer. This will take a while, and there's no output.");
-  exec.exec("\"C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vs_installer.exe\"", argumentArray);
-  core.setOutput("success", true);
-} catch (error) {
-  core.setFailed(error.message);
+const installComponents = () => {
+    try {
+        const componentArray = core.getInput("components").split(",");
+        const argumentArray = ["modify", "--installPath", "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise", "--quiet", "--norestart", "--nocache"];
+      
+        componentArray.forEach(element => {
+          console.log(`Adding ${element} to array...`);
+          argumentArray.push("--add", element);
+        });
+      
+        console.log("Launching installer. This will take a while, and there's no output.");
+        exec.exec("\"C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vs_installer.exe\"", argumentArray);
+        core.setOutput("success", true);
+      } catch (error) {
+        if (trialCount < 5) {
+            console.log("An error occurred. Attempting retry...");
+            trialCount++;
+            installComponents();
+        }
+        else {
+            core.setFailed(error.message);
+        }
+      }     
 }
